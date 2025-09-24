433ea903ed1adc880db3436568962a92563959fc
bryankinyo <bryan.quino@dbtc-cebu.edu.ph>
2025-09-24 16:18:31 +0800

    Polymorphism practice

---
+++ VehiclePractice/CarLibrary/Library/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿namespace VehicleLibrary.Library
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle (string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+        public virtual int MaxSpeed()
+        {
+            return 0;
+        }
+    }
+}
---
+++ VehiclePractice/PolymorphismPrac1/Program.cs
@@ -0,0 +1,22 @@
+﻿using VehicleLibrary.Library;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var vehicles = new List<Vehicle>
+        {
+            new Car("Toyota", 1998, 2),
+            new Bicycle("Giant", 2025, true)
+        };
+        
+        foreach (var vehicle in vehicles)
+        {
+            if ( vehicle is Car car)
+            Console.WriteLine($"Vehicle: {vehicle.GetType().Name}, Brand: {car.Brand}, Year: {car.Year}, Max Speed: {car.MaxSpeed()} km/h, Doors: {car.NumberOfDoor}");
+            
+            else if (vehicle is Bicycle bicycle)
+            Console.WriteLine($"Vehicle: {vehicle.GetType().Name}, Brand: {bicycle.Brand}, Year: {bicycle.Year}, Max Speed: {bicycle.MaxSpeed()} km/h, Has Gear: {bicycle.HasGear}");
+        }
+    }
+}
