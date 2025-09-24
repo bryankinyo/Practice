cc6e1d38cb2149e3e4c9640cb389e8a2b2aa729b
bryankinyo <bryan.quino@dbtc-cebu.edu.ph>
2025-09-24 15:45:56 +0800

    main program added.

--- ShapesCalculator/Program.cs
+++ ShapesCalculator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library.Shapes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var shapes = new List<Shapes>
+        {
+            new Circle(5),
+            new Rectangle(4, 7)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}");
+            Console.WriteLine($"Area: {shape.CalculateArea():F2}\n");
+        }
+    }
+}
