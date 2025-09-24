bce7c4dcf27e5b94fcc83eb69f6cd78c4bc22b0e
bryankinyo <bryan.quino@dbtc-cebu.edu.ph>
2025-09-24 15:31:19 +0800

    Shapes Calculator Practice

---
+++ Library/Shapes/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Shapes
+{
+    public class Circle : Shapes
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea() => Math.PI * Radius * Radius;
+
+        //public override double CalculateArea()
+        //{
+        //    return Math.PI * Radius * Radius;
+        //}
+    }
+}
---
+++ Library/Shapes/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Shapes
+{
+    public class Rectangle : Shapes
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea() => Width * Height;
+
+        //public override double CalculateArea()
+        //{
+        //    return Height * Width;
+        //}
+    }
+}
